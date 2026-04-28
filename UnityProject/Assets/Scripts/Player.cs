195817dca112c6238f59ab8888246dab42f247d3
sableangle <sableangle@gmail.com>
2026-04-28 11:56:31 +0800

    feat: add model, add animation

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
+    public float turnSpeed = 720f;
+    public Animator animator;
 
     Rigidbody rb;
     bool isGrounded;
@@ -17,7 +19,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetTrigger("Jump");
+        }
     }
 
     void FixedUpdate()
@@ -25,8 +30,41 @@ public class Player : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = new Vector3(h, 0f, v).normalized * moveSpeed;
+        Vector3 inputDirection = new Vector3(h, 0f, v);
+        Vector3 moveDirection = inputDirection.normalized;
+
+        if (Camera.main != null)
+        {
+            Vector3 cameraForward = Camera.main.transform.forward;
+            Vector3 cameraRight = Camera.main.transform.right;
+
+            cameraForward.y = 0f;
+            cameraRight.y = 0f;
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            moveDirection = (cameraForward * inputDirection.z + cameraRight * inputDirection.x).normalized;
+        }
+
+        Vector3 move = moveDirection * moveSpeed;
         rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+
+        if (moveDirection.sqrMagnitude > 0f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.fixedDeltaTime);
+        }
+
+        if (move.magnitude > 0)
+        {
+            animator.SetBool("IsWalking",true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+
     }
 
     void OnCollisionStay(Collision collision)
